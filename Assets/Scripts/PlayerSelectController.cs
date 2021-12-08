97d19eed80648c2879fc5dd9b6e140252f1770d2
FredIsACoolGuy <fredduffield@outlook.com>
2021-12-08 15:41:22 -0500

    Final Build with added Read Me
    Added a Read Me file to explain how to test the game through Steam.

--- Assets/Scripts/PlayerSelectController.cs
+++ Assets/Scripts/PlayerSelectController.cs
@@ -5,8 +5,11 @@ using Multiplayer.Lobby.Inputs;
 using Mirror;
 public class PlayerSelectController : NetworkBehaviour
 {
-    private Vector2 prevInput;
+    //this is for the player movement in game
 
+    //stores previous input
+    private Vector2 prevInput;
+    //controls for player
     private Controls controls;
 
     private Controls Controls
@@ -18,6 +21,7 @@ public class PlayerSelectController : NetworkBehaviour
         }
     }
 
+    //called on start on anything that has authority
     public override void OnStartAuthority()
     {
         enabled = true;
@@ -26,45 +30,48 @@ public class PlayerSelectController : NetworkBehaviour
         Controls.PlayerSelect.Move.canceled += ctx => ResetMovement();
     }
 
+    //only run on client
     [ClientCallback]
-
+    //enables controls
     private void OnEnable()
     {
         Controls.Enable();
     }
     [ClientCallback]
+    //disables controls
     private void OnDisable()
     {
         Controls.Disable();
     }
 
     [ClientCallback]
-
+    //calls move
     private void Update()
     {
         Move();
     }
 
     [Client]
+    //updates prevuis movement
     private void SetMovement(Vector2 movement)
     {
         prevInput = movement;
     }
 
     [Client]
-
+    //resets to zero
     private void ResetMovement()
     {
         prevInput = Vector2.zero;
     }
 
     [Client]
-
+    //moves character
     private void Move()
     {
         if (prevInput.x > 0)
         {
-            Debug.Log("Rifht");
+            Debug.Log("Right");
         }
     }
 
--- Assets/Scripts/SliderTextDisplay.cs
+++ Assets/Scripts/SliderTextDisplay.cs
@@ -9,12 +9,13 @@ public class SliderTextDisplay : MonoBehaviour
     private TMP_Text text;
 
     public Slider slider;
-    // Start is called before the first frame update
+    // get text component
     void Start()
     {
         text = this.GetComponent<TMP_Text>();
     }
 
+    //called when slider value changes
     public void SliderChange()
     {
         text.text = slider.value.ToString();
